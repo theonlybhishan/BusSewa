b472675315015b353a65e05e4bac4bf4b2defc14
theonlybhishan <bhishanpoudel17@gmail.com>
2022-09-02 11:59:17 +0545

    media file

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR/'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
